a1da9cd9787d46a3de3e1adfd509169044da2d0f
Claes Ladefoged <claesnl@gmail.com>
2023-06-09 10:45:29 +0200

    Added preprocessing, and updated to rh-node@1.1.1

--- nodes/amyloidAI/test_amyloidAI.py
+++ nodes/amyloidAI/test_amyloidAI.py
@@ -1,28 +1,31 @@
 from rhnode import RHJob
 
-data = {
-    #"PET_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0010/PET_affine_BET.nii.gz",
+data = [
+    {"PET_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0010/PET_affine_BET.nii.gz"},
+
+    {   
+    "PET_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0010/PET.nii.gz",
+    "CT_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0010/ANAT.nii.gz"},
     
-    #"PET_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0010/PET.nii.gz",
-    #"CT_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0010/ANAT.nii.gz",
-    
+    {"PET_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0023/PET_affine_BET.nii.gz"},
+    {
     "PET_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0023/PET.nii.gz",
-    "MRI_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0023/ANAT.nii.gz",
-    
-}
+    "MRI_file": "/homes/claes/projects/amyloidClassifier/Data/nii/amyloid_0023/ANAT.nii.gz"}
+]
 
-node = RHJob(
-    node_name="amyloidai",
-    inputs = data,
-    node_address="titan6.petnet.rh.dk:8010",
-    #node_address = "localhost:8010",
-    check_cache=False,
-    output_directory=".",
-)
 
-#Queue the node for execution
-node.start()
+nodes=[]
+for d in data:
+    node = RHJob(
+        node_name="amyloidai",
+        inputs = d,
+        manager_address='titan6:9050'
+    )
 
-# Saves files in cwd/node_name_[i]/}
-output = node.wait_for_finish()
-print(output)
+    #Queue the node for execution
+    node.start()
+    nodes.append(node)
+
+for ind,node in enumerate(nodes):
+    output = node.wait_for_finish()
+    print(data[ind]['PET_file'], output)

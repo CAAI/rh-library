08456df86d518ec7214fb1bdea9ad54fa19b9238
Claes Ladefoged <claesnl@gmail.com>
2023-09-06 14:22:48 +0200

    Modified

--- nodes/aims/test_aims.py
+++ nodes/aims/test_aims.py
@@ -11,7 +11,8 @@ data = {
 node = RHJob(
     node_name="aims",
     inputs = data,
-    node_address = "localhost:9050",
+    #node_address = "localhost:9050",
+    node_address = "aims:9030",
     check_cache=False,
 )
 #Queue the node for execution
@@ -34,7 +35,56 @@ data = {
 node = RHJob(
     node_name="aims",
     inputs = data,
-    node_address = "localhost:9050",
+    #node_address = "localhost:9050",
+    node_address = "aims:9030",
+    check_cache=False,
+)
+#Queue the node for execution
+node.start()
+# Saves files in cwd/node_name_[i]/}
+output = node.wait_for_finish()
+print(output)
+assert os.path.exists('aims/AIMS.nii.gz')
+shutil.rmtree('aims', ignore_errors=True)
+
+## WITH orig MODEL
+data = {
+    "flair": "/homes/claes/projects/testarea/MS_SyntheticLesions/Data/training_data/MSSEG_13322/FLAIR_synth_native.nii.gz",
+    "t2": "/homes/claes/projects/testarea/MS_SyntheticLesions/Data/training_data/MSSEG_13322/T2_synth_native.nii.gz",
+    "t1": "/homes/claes/projects/testarea/MS_SyntheticLesions/Data/training_data/MSSEG_13322/T1_synth_native.nii.gz",
+    "do_preprocess": False,
+    "model_name": 'FLAIR_T2_T1_orig',
+    "out_filename": 'AIMS.nii.gz'
+}
+node = RHJob(
+    node_name="aims",
+    inputs = data,
+    #node_address = "localhost:9050",
+    node_address = "aims:9030",
+    check_cache=False,
+)
+#Queue the node for execution
+node.start()
+# Saves files in cwd/node_name_[i]/}
+output = node.wait_for_finish()
+print(output)
+assert os.path.exists('aims/AIMS.nii.gz')
+shutil.rmtree('aims', ignore_errors=True)
+
+
+## With no T1 model
+data = {
+    "flair": "/homes/claes/projects/testarea/MS_SyntheticLesions/Data/training_data/MSSEG_13322/FLAIR_synth_native.nii.gz",
+    "t2": "/homes/claes/projects/testarea/MS_SyntheticLesions/Data/training_data/MSSEG_13322/T2_synth_native.nii.gz",
+    "do_preprocess": False,
+    "model_name": 'FLAIR_T2_orig',
+    "out_filename": 'AIMS.nii.gz'
+}
+node = RHJob(
+    node_name="aims",
+    inputs = data,
+    #node_address = "localhost:9050",
+    node_address = "aims:9030",
     check_cache=False,
 )
 #Queue the node for execution

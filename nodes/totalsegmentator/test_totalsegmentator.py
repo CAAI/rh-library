049fffa6dbcb62fa773af692b6d9257f74475a09
Christian Hinge <christian@hingepedersen.dk>
2024-03-19 17:32:18 +0100

    size-aware totalsegmentator. If more than 590 slices, use memory reducing flags. If this is not done, Totalsegmentator will hang indefinetly. This is problem is reproducible using ts outside rhnode and docker. As per https://github.com/wasserth/TotalSegmentator/issues/240 it is a problem with nnunet.

--- nodes/totalsegmentator/test_totalsegmentator.py
+++ nodes/totalsegmentator/test_totalsegmentator.py
@@ -1,24 +1,38 @@
+#from rhnode import RHJob
 from rhnode import RHJob
 
 data = {
     #"in_file": "/homes/claes/projects/LowdosePET/Cu64DOTATATE/Data/mnc/DOTA_000/CT.nii",
-    "in_file": "/homes/hinge/lymphoma/data/raw/RIS_PACS/LM12533_02/CTres_crop.nii.gz",#"/homes/claes/projects/LowdosePET/PE2I/data_Vision/nii/0rCyjRXXbm_0/ACCT.nii.gz",
+    "in_file": "/homes/hinge/Projects/rh-library/nodes/totalsegmentator/testlarge.nii.gz",#"/homes/hinge/lymphoma/data/raw/RIS_PACS/LM12533_02/CTres_crop.nii.gz",#"/homes/claes/projects/LowdosePET/PE2I/data_Vision/nii/0rCyjRXXbm_0/ACCT.nii.gz",
     'fast': True,
-    'roi_subset': 'brain skull',
-    #'out_segmentation': 'test.nii.gz'
+   # 'roi_subset': 'brain skull',
+    'out_segmentation': 'test.nii.gz'
 }
 
 node = RHJob(
     node_name="totalsegmentator",
     inputs=data,
-    manager_address="titan5:9030",
+    #manager_address="http://127.0.0.1:8009",
+    node_address="localhost:8009",
     check_cache=False,
+    resources_included=True,
+    included_cuda_device=0
 )
 # Wait for the node to finish
 node.start()
 
 output = node.wait_for_finish()
 print(output)
+
+# totalsegmentator(
+#         str(data["in_file"]), "/homes/hinge/Projects/rh-library/nodes/totalsegmentator/out.nii.gz",ml=True, nr_thr_resamp=1, nr_thr_saving=1,
+#         fast=False, nora_tag="None", preview=False, task="total", roi_subset=None,
+#         statistics=False, radiomics=False, crop_path=None, body_seg=False,
+#         force_split=False, output_type="nifti", quiet=False, verbose=False, test=0,
+#         skip_saving=False, device="gpu", license_number=None,
+#         statistics_exclude_masks_at_border=True, no_derived_masks=False,
+#         v1_order=False, fastest=False, roi_subset_robust=None
+# )
 
 # Alternatively to interrupt the job:
 # node.stop()

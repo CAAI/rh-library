977f3d2d69596c7a2e00e68ded91d17cf02844b8
Claes Ladefoged <claesnl@gmail.com>
2024-09-23 16:40:17 +0200

    Added synthseg

--- nodes/synthseg/test_synthseg.py
+++ nodes/synthseg/test_synthseg.py
@@ -1,16 +1,14 @@
 from rhnode import RHJob
 
 data = {
-    "in_file": "/homes/claes/projects/LowdosePET/PE2I/data_Vision/nii/0rCyjRXXbm_0/ACCT.nii.gz",
-    "xargs": '--ct --fast --crop 90'
+    "in_file": "/depict/data/ventricles/rawdata/sub-PAT001/ses-20210824/anat/sub-PAT001_ses-20210824_FLAIR.nii.gz",
 }
 
 node = RHJob(
     node_name="synthseg",
     inputs=data,
-    #manager_address="aims:9030",
-    node_address='localhost:9050',
-    #node_address='localhost:8010',
+    manager_address="aims:9030",
+    #node_address='localhost:9050',
     check_cache=False
 )
 # Wait for the node to finish

d1a85911aa56405c11edd4f9611a35391161fa2b
Claes Ladefoged <claesnl@gmail.com>
2024-11-21 09:09:23 +0100

    added support for b20p

--- nodes/deepdixon_2020/test_deepdixon_2020.py
+++ nodes/deepdixon_2020/test_deepdixon_2020.py
@@ -1,8 +1,10 @@
 from rhnode import RHJob
 
+# VB20
 data = {
-    "in_phase": "/depict/data/mrac/rawdata/sub-2jOpUJ7SHl/ses-20201221/anat/sub-2jOpUJ7SHl_ses-20201221_in.nii.gz",
-    "out_phase": "/depict/data/mrac/rawdata/sub-2jOpUJ7SHl/ses-20201221/anat/sub-2jOpUJ7SHl_ses-20201221_opp.nii.gz"
+    "in_phase": "/depict/data/mrac/rawdata/sub-1tIv2WT0Ws/ses-20140829/anat/sub-1tIv2WT0Ws_ses-20140829_in.nii.gz",
+    "out_phase": "/depict/data/mrac/rawdata/sub-1tIv2WT0Ws/ses-20140829/anat/sub-1tIv2WT0Ws_ses-20140829_opp.nii.gz",
+    'mMR_version': 'B20P'
 }
 
 node = RHJob(
@@ -13,4 +15,37 @@ node = RHJob(
 )
 node.start()
 node.wait_for_finish()
+
+# VE11
+
+data = {
+    "in_phase": "/depict/data/mrac/rawdata/sub-2jOpUJ7SHl/ses-20201221/anat/sub-2jOpUJ7SHl_ses-20201221_in.nii.gz",
+    "out_phase": "/depict/data/mrac/rawdata/sub-2jOpUJ7SHl/ses-20201221/anat/sub-2jOpUJ7SHl_ses-20201221_opp.nii.gz",
+    'mMR_version': 'E11'
+}
+
+node = RHJob(
+    node_name="deepdixon_2020",
+    #node_address='localhost:9080',
+    node_address='titan6:9030',
+    inputs = data,
+)
+node.start()
+node.wait_for_finish()
+
+
+data = {
+    "in_phase": "/depict/data/mrac/rawdata/sub-2jOpUJ7SHl/ses-20201221/anat/sub-2jOpUJ7SHl_ses-20201221_in.nii.gz",
+    "out_phase": "/depict/data/mrac/rawdata/sub-2jOpUJ7SHl/ses-20201221/anat/sub-2jOpUJ7SHl_ses-20201221_opp.nii.gz",
+}
+
+node = RHJob(
+    node_name="deepdixon_2020",
+    #node_address='localhost:9080',
+    node_address='titan6:9030',
+    inputs = data,
+)
+node.start()
+node.wait_for_finish()
+
 

2809c176d383e430c478af7523f6554e1d6c6f83
Claes Ladefoged <claesnl@gmail.com>
2024-11-21 07:11:56 +0100

    increased memory for deepdixon

--- nodes/deepdixon_2020/deepdixon_2020.py
+++ nodes/deepdixon_2020/deepdixon_2020.py
@@ -106,9 +106,9 @@ class DeepDixonNode(RHNode):
     input_spec = DeepDixonInput
     output_spec = DeepDixonOutput
     name = "deepdixon_2020"
-    required_gb_gpu_memory = 6
+    required_gb_gpu_memory = 12
     required_num_threads = 2
-    required_gb_memory = 8
+    required_gb_memory = 12
 
     def process(inputs,
                 job):
